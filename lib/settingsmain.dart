29e212f9359d3bfe0719e4c1d3084154e62f28bf
ser356 <70541693+ser356@users.noreply.github.com>
2023-05-28 18:49:17 +0200

    hola

---
+++ lib/settingsmain.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+import "package:sinhex/hex.dart";
+
+class SettingsMain extends StatelessWidget {
+  @override
+  const SettingsMain({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: '222222'.toColor(),
+      ),
+    );
+  }
+}
